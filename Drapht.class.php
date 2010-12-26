45a0e03ef05a63a0c91120f5134ffb43217ea039
andy <andychase@gmail.com>
2010-12-26 11:53:08 -0500

    Added support for batch setting warp pattern, weft pattern, and master pattern (with built-in tabby and twill support).  Also added pattern export method.

--- Drapht.class.php
+++ Drapht.class.php
@@ -1,5 +1,9 @@
 <?php
 
+define('DRAPHT_PATTERN_TABBY', 1);
+define('DRAPHT_PATTERN_TWILL', 2);
+define('DRAPHT_PATTERN_CUSTOM', 3);
+
 /**
  * PHP class for building and rendering weaving drafts.
  *
@@ -27,6 +31,89 @@ class Drapht {
 
   public function tie($treadle, $harnesses = array()) {
     $this->treadles[$treadle] = $harnesses;
+  }
+
+  public function setWarpPattern($harnesses) {
+    //Set warp harnesses
+    $h = 0;
+    for ($i = 0; $i < count($this->warp); $i++) {
+      $this->warp[$i]->setHarness($harnesses[$h]);
+      $h++;
+      if ($h == count($harnesses)) {
+        $h = 0;
+      }
+    }
+  }
+
+  public function setWeftPattern($treadles) {
+    //Set weft treadles
+    $t = 0;
+    for ($i = 0; $i < count($this->weft); $i++) {
+      $this->weft[$i]->setTreadle($treadles[$t]);
+      $t++;
+      if ($t == count($treadles)) {
+        $t = 0;
+      }
+    }
+  }
+
+  public function setPattern($type, $custom = NULL) {
+    switch($type) {
+      case DRAPHT_PATTERN_TABBY:
+        $pattern = array(
+          'warp' => array(1,2),
+          'weft' => array(1,2),
+          'tie'  => array(
+            1 => array(1),
+            2 => array(2),
+          )
+        );
+        break;
+      case DRAPHT_PATTERN_TWILL:
+        $pattern = array(
+          'warp' => array(1,2,3,4),
+          'weft' => array(1,2,3,4),
+          'tie' => array(
+            1 => array(1,2),
+            2 => array(2,3),
+            3 => array(3,4),
+            4 => array(4,1),
+          ),
+        );
+        break;
+      case DRAPHT_PATTERN_CUSTOM:
+        $pattern = $custom;
+    }
+
+    $this->setWarpPattern($pattern['warp']);
+    $this->setWeftPattern($pattern['weft']);
+
+    //Tie-ups
+    foreach ($pattern['tie'] as $treadle => $harnesses) {
+      $this->tie($treadle, $harnesses);
+    }
+  }
+
+  /**
+   * Returns an array representing the warp, weft, and treddle tie-up patterns
+   * used by this draft, in the same format used by the setPattern() method; can
+   * be used to export/import patterns.
+   * @return <type>
+   */
+  public function getPattern() {
+    $warp = array();
+    $weft = array();
+    for ($i = 0; $i < count($this->warp); $i++) {
+      $warp[] = $this->warp[$i]->harness;
+    }
+    for ($i = 0; $i < count($this->weft); $i++) {
+      $weft[] = $this->weft[$i]->treadle;
+    }
+    return array(
+      'warp' => $warp,
+      'weft' => $weft,
+      'tie' => $this->treadles,
+    );
   }
 
   /**
@@ -89,9 +176,11 @@ class WarpThread {
   //public $harness;
   public $harness;
 
-  function __construct(&$color, $harness) {
+  function __construct(&$color, $harness = NULL) {
     $this->color = $color;
-    $this->harness = $harness;
+    if ($harness) {
+      $this->setHarness($harness);
+    }
   }
 
   function setHarness($newHarness) {
@@ -108,9 +197,11 @@ class WeftThread {
   //The treadle(s) to be used on this weft
   public $treadle;
 
-  function __construct(&$color, $treadle) {
+  function __construct(&$color, $treadle = NULL) {
     $this->color = $color;
-    $this->treadle = $treadle;
+    if ($treadle) {
+      $this->setTreadle($treadle);
+    }
   }
 
   function setTreadle($newTreadle) {
