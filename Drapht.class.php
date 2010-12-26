c2adfcd7b67cbffc9eb5ff3f6607f5c026a4ed45
andy <andychase@gmail.com>
2010-12-26 13:10:31 -0500

    Updated license & added warp/weft stripe methods

--- Drapht.class.php
+++ Drapht.class.php
@@ -2,31 +2,10 @@
 
 /**
  * Drapht is a set of PHP classes for building and rendering weaving drafts.
- */
-
-
-/**
- * The MIT License
  *
- * Copyright (c) 2010 Andy Chase <andychase@gmail.com>
+ * (c) 2010 Andy Chase <andychase@gmail.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * Licensed under the GPL (See LICENSE.txt for details)
  */
 
 define('DRAPHT_PATTERN_TABBY', 1);
@@ -54,8 +33,58 @@ class Drapht {
     $this->warp[count($this->warp)] = $thread;
   }
 
+  /**
+   * Add multiple threads to the warp. If $pattern is empty, the harness
+   * threading pattern will need to be set later.
+   * 
+   * @param <type> $color
+   * @param <type> $width 
+   */
+  public function addWarpStripe($color, $width, $pattern = null) {
+    if ($pattern) {
+      $h = 0;
+      for ($i = 0; $i < $width; $i++) {
+        $this->addWarpThread(new WarpThread($color, $pattern[$h]));
+        $h++;
+        if ($h == count($pattern)) {
+          $h = 0;
+        }
+      }
+    }
+    else {
+      for ($i = 0; $i < $width; $i++) {
+        $this->addWarpThread(new WarpThread($color));
+      }
+    }
+  }
+
   public function addWeftThread($thread) {
     $this->weft[count($this->weft)] = $thread;
+  }
+
+  /**
+   * Add multiple threads to the weft. If $pattern is empty, the weft
+   * treadle pattern will need to be set later.
+   *
+   * @param <type> $color
+   * @param <type> $width
+   */
+  public function addWeftStripe($color, $width, $pattern = null) {
+    if ($pattern) {
+      $t = 0;
+      for ($i = 0; $i < $width; $i++) {
+        $this->addWeftThread(new WeftThread($color, $pattern[$t]));
+        $t++;
+        if ($t == count($pattern)) {
+          $t = 0;
+        }
+      }
+    }
+    else {
+      for ($i = 0; $i < $width; $i++) {
+        $this->addWeftThread(new WeftThread($color));
+      }
+    }
   }
 
   public function tie($treadle, $harnesses = array()) {
